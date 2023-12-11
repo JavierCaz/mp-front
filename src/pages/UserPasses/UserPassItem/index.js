ada3e50bf0bc3719252d3ce76d2c79c8d11f2417
Javier Cazares <javiercaz98@hotmail.com>
2023-12-10 19:58:05 -0700

    Add favicon to pass card

--- src/pages/UserPasses/UserPassItem/index.js
+++ src/pages/UserPasses/UserPassItem/index.js
@@ -7,11 +7,18 @@ import { styled } from '@mui/material/styles';
 import { Delete, Edit, Star } from '@mui/icons-material';
 
 
-const EllipsisTypography = styled(Typography)(({ lines = 1 }) => ({
+const EllipsisText = styled(Typography)(({ lines = 1 }) => ({
     'display': '-webkit-box',
     'WebkitLineClamp': lines,
     'WebkitBoxOrient': 'vertical',
     'overflow': 'hidden'
+}))
+
+const EllipsisTextLine = styled(Typography)(({ maxWidth = '200px' }) => ({
+    'overflow': 'hidden',
+    'textOverflow': 'ellipsis',
+    'whiteSpace': 'nowrap',
+    'maxWidth': maxWidth,
 }))
 
 const UserPassItem = (props) => {
@@ -24,28 +31,52 @@ const UserPassItem = (props) => {
         handleDelete,
     } = props
 
+    const getFaviconDomainFromUri = (uri) => {
+        let domain = uri;
+        
+        // Find & remove protocol (http, ftp, etc.) and get domain
+        if (domain.indexOf("://") > -1) {
+          domain = domain.split('/')[2];
+        } else {
+          domain = domain.split('/')[0];
+        }
+        
+        // Find & remove port number
+        domain = domain.split(':')[0];
+        
+        return 'http://www.google.com/s2/favicons?domain=' + domain;
+    }
+
     return (
         <Grid item xs sm="auto" sx={{ maxWidth: { sm: 400 } }}>
             <Card sx={{ display: 'flex', flexDirection: { xs: 'column', sm: 'row' } }}>
                 <CardContent sx={{ flex: '1' }}>
-                    <Typography gutterBottom variant="subtitle1" component="div">
-                        {name} {favorite && <Star fontSize="small" color="primary" />}
-                    </Typography>
+                    
+                    <Grid container alignItems="center">
+                        <Grid item>
+                            <Typography gutterBottom variant="subtitle1" component="div">
+                                {name}
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            {favorite && <Star fontSize="small" color="primary" sx={{marginLeft: '3px'}}/>}
+                        </Grid>
+                    </Grid>
 
-                    <Typography variant="body2" gutterBottom
-                        sx={{
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            whiteSpace: 'nowrap',
-                            maxWidth: '200px',
-                        }}
-                    >
-                        {uri}
-                    </Typography>
+                    <Grid container alignItems="center">
+                        <Grid item>
+                            <img height="18" width="18" src={getFaviconDomainFromUri(uri)} style={{marginRight: '5px'}} />
+                        </Grid>
+                        <Grid item>
+                            <EllipsisTextLine variant="body2" gutterBottom>
+                                {uri}
+                            </EllipsisTextLine>
+                        </Grid>
+                    </Grid>
 
-                    <EllipsisTypography variant="body2" color="text.secondary" lines="2">
+                    <EllipsisText variant="body2" color="text.secondary" lines="2">
                         {notes}
-                    </EllipsisTypography>
+                    </EllipsisText>
                 </CardContent>
 
                 <Box sx={{ display: 'flex', flexDirection: { sm: 'column' }, justifyContent: { xs: 'flex-end' } }}>
